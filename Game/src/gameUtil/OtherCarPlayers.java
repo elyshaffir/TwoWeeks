727a5fb77afcc8041af25d19731dd6c07db519f8
elyshaffir <elyshaffir@gmail.com>
2018-05-04 12:44:14 +0300

    Beta 1.3
    Performance highly enhanced.

--- Game/src/gameUtil/OtherCarPlayers.java
+++ Game/src/gameUtil/OtherCarPlayers.java
@@ -4,19 +4,16 @@ import gameCom.Client;
 import org.lwjgl.util.vector.Vector3f;
 import renderEngine.Loader;
 import renderEngine.MasterRenderer;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Objects;
 
 public class OtherCarPlayers {
     private static Client client;
-    private static String lastDataToSend = "";
+    private static String lastDataToSend = "+";
     private static HashMap<Integer, CarPlayer> otherCars = new HashMap<>();
 
     public static void sendCar(CarPlayer car, int id){
+
         String dataToSend = "";
         dataToSend += car.getChasiModelString() + "_";
         dataToSend += car.getChasiTextureString() + "_";
@@ -50,6 +47,17 @@ public class OtherCarPlayers {
 
         dataToSend += id;
 
+        String[] lastDataToSendComponents = lastDataToSend.split("_");
+        String[] dataToSendComponents = dataToSend.split("_");
+
+        try{
+            for (int index = 0; index < 22; index++)
+                if (!Objects.equals(lastDataToSendComponents[index], lastDataToSendComponents[index]))
+                    dataToSendComponents[index] = "x";
+        } catch (IndexOutOfBoundsException ignored){}
+
+        dataToSend = String.join("_", dataToSendComponents);
+
         if (!Objects.equals(dataToSend, lastDataToSend)) {
             client.setDataToSend(dataToSend);
             lastDataToSend = dataToSend;
@@ -59,6 +67,138 @@ public class OtherCarPlayers {
     }
 
     public static void loadAllOtherCars(Loader loader){
+        try{
+            String[] allCars = client.getDataFromServer().substring(1, client.getDataFromServer().length() - 1).split("###");
+            for (String currentCar:allCars){
+                if (!Objects.equals(currentCar.charAt(0), 'X')){
+                    String[] components = currentCar.split("_");
+                    int carID = Integer.parseInt(components[22]);
+                    if (otherCars.keySet().contains(carID)){ // if car already exists
+                        CarPlayer carToModify = otherCars.get(carID);
+
+                        // to modify car chasi, add adaptation for components[0] and components[1].
+
+                        if (!Objects.equals(components[2], "x")){
+                            float newX = Float.parseFloat(components[2]);
+                            Vector3f newPosition = new Vector3f(newX, carToModify.getPlayer().getPosition().y, carToModify.getPlayer().getPosition().z);
+                            carToModify.getPlayer().setPosition(newPosition);
+                        }
+
+                        if (!Objects.equals(components[3], "x")){
+                            float newY = Float.parseFloat(components[3]);
+                            Vector3f newPosition = new Vector3f(carToModify.getPlayer().getPosition().x, newY, carToModify.getPlayer().getPosition().z);
+                            carToModify.getPlayer().setPosition(newPosition);
+                        }
+
+                        if (!Objects.equals(components[4], "x")){
+                            float newZ = Float.parseFloat(components[4]);
+                            Vector3f newPosition = new Vector3f(carToModify.getPlayer().getPosition().x, carToModify.getPlayer().getPosition().y, newZ);
+                            carToModify.getPlayer().setPosition(newPosition);
+                        }
+
+                        if (!Objects.equals(components[7], "x")){
+                            float newX = Float.parseFloat(components[7]);
+                            Vector3f newPosition = new Vector3f(newX, carToModify.getFrontWheels().getPosition().y, carToModify.getFrontWheels().getPosition().z);
+                            carToModify.getFrontWheels().setPosition(newPosition);
+                        }
+
+                        if (!Objects.equals(components[8], "x")){
+                            float newY = Float.parseFloat(components[8]);
+                            Vector3f newPosition = new Vector3f(carToModify.getFrontWheels().getPosition().x, newY, carToModify.getFrontWheels().getPosition().z);
+                            carToModify.getFrontWheels().setPosition(newPosition);
+                        }
+
+                        if (!Objects.equals(components[9], "x")){
+                            float newZ = Float.parseFloat(components[9]);
+                            Vector3f newPosition = new Vector3f(carToModify.getFrontWheels().getPosition().x, carToModify.getFrontWheels().getPosition().y, newZ);
+                            carToModify.getFrontWheels().setPosition(newPosition);
+                        }
+
+                        if (!Objects.equals(components[10], "x")){
+                            float newX = Float.parseFloat(components[10]);
+                            Vector3f newPosition = new Vector3f(newX, carToModify.getBackWheels().getPosition().y, carToModify.getBackWheels().getPosition().z);
+                            carToModify.getBackWheels().setPosition(newPosition);
+                        }
+
+                        if (!Objects.equals(components[11], "x")){
+                            float newY = Float.parseFloat(components[11]);
+                            Vector3f newPosition = new Vector3f(carToModify.getBackWheels().getPosition().x, newY, carToModify.getBackWheels().getPosition().z);
+                            carToModify.getBackWheels().setPosition(newPosition);
+                        }
+
+                        if (!Objects.equals(components[12], "x")){
+                            float newZ = Float.parseFloat(components[12]);
+                            Vector3f newPosition = new Vector3f(carToModify.getBackWheels().getPosition().x, carToModify.getBackWheels().getPosition().y, newZ);
+                            carToModify.getBackWheels().setPosition(newPosition);
+                        }
+
+                        if (!Objects.equals(components[13], "x")){
+                            carToModify.getFrontWheels().setRotX(Float.parseFloat(components[13]));
+                        }
+
+                        if (!Objects.equals(components[14], "x")){
+                            carToModify.getFrontWheels().setRotY(Float.parseFloat(components[14]));
+                        }
+
+                        if (!Objects.equals(components[15], "x")){
+                            carToModify.getFrontWheels().setRotZ(Float.parseFloat(components[15]));
+                        }
+
+                        if (!Objects.equals(components[16], "x")){
+                            carToModify.getBackWheels().setRotX(Float.parseFloat(components[16]));
+                        }
+
+                        if (!Objects.equals(components[17], "x")){
+                            carToModify.getBackWheels().setRotY(Float.parseFloat(components[17]));
+                        }
+
+                        if (!Objects.equals(components[18], "x")){
+                            carToModify.getBackWheels().setRotZ(Float.parseFloat(components[18]));
+                        }
+
+                        if (!Objects.equals(components[19], "x")){
+                            carToModify.getPlayer().setRotX(Float.parseFloat(components[19]));
+                        }
+
+                        if (!Objects.equals(components[20], "x")){
+                            carToModify.getPlayer().setRotY(Float.parseFloat(components[20]));
+                        }
+
+                        if (!Objects.equals(components[21], "x")){
+                            carToModify.getPlayer().setRotZ(Float.parseFloat(components[21]));
+                        }
+                    } else{
+                        CarPlayer newCar = new CarPlayer(
+                                loader,
+                                components[0],
+                                components[1],
+                                new Vector3f(Float.parseFloat(components[2]), Float.parseFloat(components[3]), Float.parseFloat(components[4])),
+                                components[5],
+                                components[6],
+                                new Vector3f(Float.parseFloat(components[7]), Float.parseFloat(components[8]), Float.parseFloat(components[9])),
+                                new Vector3f(Float.parseFloat(components[10]), Float.parseFloat(components[11]), Float.parseFloat(components[12]))
+                        );
+
+                        newCar.getFrontWheels().setRotX(Float.parseFloat(components[13]));
+                        newCar.getFrontWheels().setRotY(Float.parseFloat(components[14]));
+                        newCar.getFrontWheels().setRotZ(Float.parseFloat(components[15]));
+
+                        newCar.getBackWheels().setRotX(Float.parseFloat(components[16]));
+                        newCar.getBackWheels().setRotY(Float.parseFloat(components[17]));
+                        newCar.getBackWheels().setRotZ(Float.parseFloat(components[18]));
+
+                        newCar.getPlayer().setRotX(Float.parseFloat(components[19]));
+                        newCar.getPlayer().setRotY(Float.parseFloat(components[20]));
+                        newCar.getPlayer().setRotZ(Float.parseFloat(components[21]));
+
+                        otherCars.put(carID, newCar);
+                    }
+                }
+            }
+        } catch (IndexOutOfBoundsException ignored){}
+    }
+
+    public static void loadAllOtherCarsOld(Loader loader){
         try{
             if (client.getDataFromServer().charAt(0) == '{' && client.getDataFromServer().charAt(client.getDataFromServer().length() - 1) == '}'){
                 String[] allCars = client.getDataFromServer().substring(1, client.getDataFromServer().length() - 1).split("###");
